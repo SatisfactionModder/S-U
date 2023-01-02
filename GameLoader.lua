1e32d5a9cd424db23fba2ddec7f35b3c5c3a2aba
SatisfactionModder <116923555+SatisfactionModder@users.noreply.github.com>
2023-01-02 09:38:50 +0000

    Update GameLoader.lua

--- GameLoader.lua
+++ GameLoader.lua
@@ -37,10 +37,8 @@ elseif table.find(EclipseList, game.PlaceId) then
   
   
     -- Eclipse Hub
-    getgenv().mainKey = "nil";
-
-    local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
-
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SatisfactionModder/S-U/main/Eclipse.lua"))()  
+ 
 else
      Notification.WallNotification("Game Loader", "Unable To Find Current Game!", {
     MainSettings = {
